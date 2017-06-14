d6f98d2f99671e2f9348ec1b100db88e0ab7bd57
sdelamo <sergio.delamo@softamo.com>
2017-06-14 13:52:41 +0200

    Remove assets created by Grails 3 create-app

--- grails-app/assets/javascripts/metadataexchange/metadataexchange.js
+++
@@ -1,9 +0,0 @@
-//= wrapped
-//= require /angular/angular
-//= require /metadataexchange/core/metadataexchange.core
-//= require /metadataexchange/index/metadataexchange.index
-
-angular.module("metadataexchange", [
-        "metadataexchange.core",
-        "metadataexchange.index"
-    ]);
