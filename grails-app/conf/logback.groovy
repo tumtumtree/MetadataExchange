67d4c5f8916bad111ffc95f91eb57b559f9b49b4
James Dai <james.dai.uk@googlemail.com>
2017-06-15 14:20:32 +0100

    MET-1849 wrote logback.groovy config file to match old log4j section of Config.groovy

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -22,6 +22,7 @@ appender('STDOUT', ConsoleAppender) {
     }
 }
 
+// storing stacktrace in target in development mode:
 def targetDir = BuildSettings.TARGET_DIR
 if (Environment.isDevelopmentMode() && targetDir != null) {
     appender("FULL_STACKTRACE", FileAppender) {
@@ -31,6 +32,66 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
             pattern = "%level %logger - %msg%n"
         }
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false) //
 }
+
+//// Setting log levels for various loggers:
+
+// Don't know if all of these names are correct for Grails 3?
+
+/// grails-app loggers:
+logger('grails.app.services.org.modelcatalogue', INFO) // set log level to INFO to all Services in package org.modelcatalogue
+logger('grails.app.controllers.org.modelcatalogue', INFO)
+logger('grails.app.domain.org.modelcatalogue', INFO)
+logger('grails.app.services.org.grails.plugins.console', INFO)
+logger('grails.app.services.org.modelcatalogue.core.elasticsearch', INFO)
+logger('org.grails.plugins.console', INFO)
+
+
+
+//    logger('org.codehaus.groovy.grails.web.mapping', DEBUG)
+//    logger('org.springframework.security', DEBUG)
+//    logger('org.grails.plugins.elasticsearch', DEBUG)
+
+//    if (Environment.current == Environment.DEVELOPMENT || Environment.current == Environment.CUSTOM) {
+//        logger('org.hibernate.type', TRACE)
+//        logger('org.hibernate.stat', TRACE)
+//        logger('org.hibernate.SQL', DEBUG)
+//    }
+
+/// other grails features loggers:
+// note org.grails.x are internal APIs, grails.x are external APIs.
+
+logger('grails.web.pages', ERROR)               // GSP
+logger('org.grails.web.sitemesh', ERROR)        // layouts
+logger('org.grails.web.servlet', ERROR)         // controllers
+logger('grails.web.servlet', ERROR)             // controllers (external)
+logger('org.grails.web.mapping', ERROR)         // URL mapping
+logger('grails.web.mapping', ERROR)             // URL mapping (external)
+logger('org.grails.commons', ERROR)             // core / classloading
+logger('org.grails.plugins', ERROR)             // plugins
+logger('grails.plugins', ERROR)                 // plugins (external)
+
+logger('org.grails.orm.hibernate', ERROR)      // hibernate integration
+logger('org.springframework', ERROR)
+logger('org.hibernate', ERROR)
+logger('net.sf.ehcache.hibernate', ERROR)
+
+/// groovy source loggers:
+logger('org.modelcatalogue', INFO)
+logger('org.modelcatalogue.core.dataarchitect.xsd.XSDImporter', INFO)
+
+// detailed feedback is now visible using the ProgressMonitor API
+logger('org.modelcatalogue.core.util.builder', INFO)
+logger('org.modelcatalogue.core.util.HibernateHelper', INFO) // for some reason the logging from builder is redirected here, INFO)
+
+logger('org.modelcatalogue.core.publishing', INFO)
+logger('org.modelcatalogue.core.util.test', INFO)
+logger('org.modelcatalogue.core.gel', INFO)
+logger('org.modelcatalogue.core.export', INFO)
+logger('org.modelcatalogue.core.elasticsearch', INFO)
+logger('org.modelcatalogue.discourse', INFO)
+logger('org.modelcatalogue.core.rx.BatchOperator', INFO)
+logger('org.modelcatalogue.core.rx.DetachedCriteriaOnSubscribe', INFO)
+
 root(ERROR, ['STDOUT'])
