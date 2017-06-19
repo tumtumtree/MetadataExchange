c9f7362bc8f9d0c1ca1f0ef71c9f6295ea6acc94
sdelamo <sergio.delamo@softamo.com>
2017-06-19 11:16:08 +0200

    Already defined in application.yml

--- ModelCatalogueCorePluginTestApp/grails-app/conf/Config.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/Config.groovy
@@ -9,8 +9,6 @@ oauth.providers = [:]
 
 grails.app.context = '/'
 
-// The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
-grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
 grails.mime.types = [ // the first one is the default format
                       atom         : 'application/atom+xml',
                       css          : 'text/css',
