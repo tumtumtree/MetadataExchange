7111231825a01a190b66a1e24fdbabb2d84c1233
sdelamo <sergio.delamo@softamo.com>
2017-06-23 15:26:07 +0200

    Migrate hibernate config

--- ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
@@ -1,10 +1,3 @@
-hibernate {
-    cache.use_second_level_cache    = true
-    cache.use_query_cache           = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-//    documentCache.region.factory_class = 'org.hibernate.documentCache.ehcache.EhCacheRegionFactory' // Hibernate 4
-}
-
 // environment specific settings
 environments {
     // XXX: never commit your local configuration overrides for this file!!!
