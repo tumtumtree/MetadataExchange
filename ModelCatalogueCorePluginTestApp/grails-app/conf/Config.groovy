dec3a5e0ce77daa8253faf0bd47b598db972f96b
sdelamo <sergio.delamo@softamo.com>
2017-06-23 13:27:46 +0200

    Remove GSP configuration settings
    Defaults in Grails 3:
    
    grails.views.gsp.encoding: UTF-8
    grails.views.gsp.htmlcodec: xml
    grails.views.gsp.codecs.expression: html
    grails.views.gsp.codecs.taglib: none
    grails.views.gsp.codecs.staticparts: none

--- ModelCatalogueCorePluginTestApp/grails-app/conf/Config.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/Config.groovy
@@ -10,31 +10,8 @@ oauth.providers = [:]
 // URL Mapping Cache Max Size, defaults to 5000
 //grails.urlmapping.documentCache.maxsize = 1000
 
-// GSP settings
-grails {
-    views {
-        gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
-            codecs {
-                expression = 'html' // escapes values inside ${}
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
-            }
-        }
-        // escapes all not-encoded output at final stage of outputting
-        filteringCodecForContentType {
-            //'text/html' = 'html'
-        }
-    }
-}
-
 // packages to include in Spring bean scanning
 grails.spring.bean.packages = []
 
-grails.views.gsp.encoding = "UTF-8"
-
 grails.assets.babel.enabled = true
 grails.assets.less.compiler = 'less4j'
-
-
