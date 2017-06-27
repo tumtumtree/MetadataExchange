45080fa89c8cf79a3188f11db8623fdb852a646c
sdelamo <sergio.delamo@softamo.com>
2017-06-27 10:02:14 +0200

    No longer applicable for Grails 3

--- grails-app/services/org/modelcatalogue/core/PerformanceUtilService.groovy
+++ grails-app/services/org/modelcatalogue/core/PerformanceUtilService.groovy
@@ -1,18 +1,16 @@
 package org.modelcatalogue.core
 
-import org.grails.plugins.domain.DomainClassGrailsPlugin
+import groovy.transform.CompileStatic
 import org.hibernate.SessionFactory
 
+@CompileStatic
 class PerformanceUtilService {
 
-
     SessionFactory sessionFactory
-    def propertyInstanceMap = DomainClassGrailsPlugin.PROPERTY_INSTANCE_MAP
 
     def cleanUpGorm() {
         def session = sessionFactory.currentSession
         session.flush()
         session.clear()
-        propertyInstanceMap.get().clear()
     }
 }
