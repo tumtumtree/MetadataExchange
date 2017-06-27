c02c8444d2d3333f33556f5b2c2435b420cf7412
sdelamo <sergio.delamo@softamo.com>
2017-06-27 10:58:41 +0200

    Add explicitely getter and setter to avoid compilation problems

--- src/main/groovy/org/modelcatalogue/core/LogoutListeners.groovy
+++ src/main/groovy/org/modelcatalogue/core/LogoutListeners.groovy
@@ -1,11 +1,20 @@
 package org.modelcatalogue.core
 
+import groovy.transform.CompileStatic
 import org.modelcatalogue.core.security.User
 
+@CompileStatic
 public trait LogoutListeners {
 
     List<LogoutListener> listeners = []
 
+    List<LogoutListener> getListeners() {
+        return this.listeners
+    }
+
+    void setListeners(List<LogoutListener> listeners) {
+        this.listeners = listeners
+    }
 
     void addLogoutListener(LogoutListener listener) {
         listeners << listener
