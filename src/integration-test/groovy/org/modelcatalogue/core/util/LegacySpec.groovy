a6fb2c44faef4f9e1aa8386fe174888114ffde58
James Dai <james.dai.uk@googlemail.com>
2017-06-28 15:07:43 +0100

    MET-1883 added GrailsConfigurationAware/setConfiguration to Legacy and changed LegacySpec to use GStrings in where

--- src/integration-test/groovy/org/modelcatalogue/core/util/LegacySpec.groovy
+++ src/integration-test/groovy/org/modelcatalogue/core/util/LegacySpec.groovy
@@ -10,22 +10,20 @@ class LegacySpec extends Specification {
 
     @Unroll
     def "fixes model catalogue id #original to #fixed"() {
-        Object old = Holders.grailsApplication.config.grails.serverURL
-        Holders.grailsApplication.config.grails.serverURL = "http://localhost:8080"
+        String serverUrl = Legacy.serverUrl
         expect:
         Legacy.fixModelCatalogueId(original) == fixed
 
 
         cleanup:
-        Holders.grailsApplication.config.grails.serverURL = old
 
         where:
         original                                                | fixed
         null                                                    | null
-        'http://localhost:8080/catalogue/classification/24.1'   | 'http://localhost:8080/catalogue/dataModel/24.1'
-        'http://localhost:8080/catalogue/model/22.1'            | 'http://localhost:8080/catalogue/dataClass/22.1'
+        "${serverUrl}/catalogue/classification/24.1"            | "${serverUrl}/catalogue/dataModel/24.1"
+        "${serverUrl}/catalogue/model/22.1"                     | "${serverUrl}/catalogue/dataClass/22.1"
         'http://www.example/model/ka'                           | 'http://www.example/model/ka'
-        'http://localhost:8080/catalogue/dataElement/22.1'      | 'http://localhost:8080/catalogue/dataElement/22.1'
+        "${serverUrl}/catalogue/dataElement/22.1"               | "${serverUrl}catalogue/dataElement/22.1"
 
     }
 
